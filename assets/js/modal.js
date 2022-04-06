a3b5a750452db90f8f4d9c54f5e7dc5e9814ea41
Yauheni88 <86955351+Yauheni88@users.noreply.github.com>
2022-04-07 00:09:41 +0300

    js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,6 +2,7 @@ const button = document.querySelector('[data-modal-button]');
 const modal = document.querySelector('[data-modal]');
 const buttonClose = document.querySelector('[data-modal-close]');
 const nav = document.querySelector('[data-modal-menu]');
+const links = document.querySelectorAll('.adaptive-link');
 
 button.addEventListener('click', function() {
     modal.classList.remove('menu-close');
@@ -20,4 +21,11 @@ modal.addEventListener('click', function() {
 
 nav.addEventListener('click', function(e) {
     e.stopPropagation();
+})
+
+links.forEach(function(item) {
+    item.addEventListener('click', function() {
+        modal.classList.add('menu-close');
+        nav.classList.add('nav-close');
+    })
 })
