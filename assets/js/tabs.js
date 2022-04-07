b5f13d4357706345e31a5baf66014c0809a7afdf
Yauheni88 <86955351+Yauheni88@users.noreply.github.com>
2022-04-07 17:11:21 +0300

    1

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -3,6 +3,10 @@ const contentBoxes = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(function(item) {
     item.addEventListener('click', function() {
+        tabs.forEach(function(item) {
+            item.classList.remove('portfolio-active');
+        })
+        this.classList.add('portfolio-active');
         const contentBox = document.querySelector('#' + this.dataset.tab);
         contentBoxes.forEach(function(item) {
             item.classList.add('tab-hidden');
